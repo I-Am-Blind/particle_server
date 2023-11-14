44775ebb79992736b355d9aeff56ab168954bf5b
Amjad Ali <amjadali.eth@gmail.com>
2023-11-14 21:33:00 +0530

    new endpoints

--- app.py
+++ app.py
@@ -7,16 +7,18 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+data = ''
 
 @app.route('/hello', methods=['POST'])
 def hello():
     data = request.get_json()
-    print(data)
+    
     url = "https://api.particle.io/v1/devices/events?access_token=6150bed281fa3139051d52f44f4cade23b93dbdc"
-
+    if not data: 
+        return "telegram bot error",201
     payload = json.dumps({
     "name": "telegramUpdate",
-    "data": "new update"
+    "data": f"{data['message']['text']},{data['via_bot']['id']}"
     })
     headers = {
    'Content-Type': 'application/json'
@@ -24,6 +26,10 @@ def hello():
 
     response = requests.request("POST", url, headers=headers, data=payload)
     return "hello", 200
+
+@app.route('/getdata')
+def getdata():
+    return data,200
 
 if __name__ == '__main__':
     app.run()
