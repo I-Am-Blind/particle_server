d8ac7d5374e18dfe5a36401e74366b4b937ea255
Amjad Ali <amjadali.eth@gmail.com>
2023-11-14 21:42:51 +0530

    new changes

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import requests 
+import logging
 from flask import Flask, request, json
 from flask_cors import CORS
 
@@ -7,10 +8,12 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+logging.basicConfig(filename='flask_app.log', level=logging.DEBUG)
 
 @app.route('/hello', methods=['POST'])
 def hello():
     data = request.get_json()
+    app.logger.info(data)
     url = "https://api.particle.io/v1/devices/events?access_token=6150bed281fa3139051d52f44f4cade23b93dbdc"
     if not data: 
         return "telegram bot error",201
@@ -23,7 +26,7 @@ def hello():
      }
 
     response = requests.request("POST", url, headers=headers, data=payload)
-    return data, 200
+    return "hello", 200
 
 
 if __name__ == '__main__':
