784e3a47131f34271be108dd9ef504db7c2ce0c6
Amjad Ali <amjadali.eth@gmail.com>
2023-11-15 20:10:22 +0530

    made registration page for one click webhook setup

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import requests
 from loguru import logger 
 from flask import Flask, request, json ,render_template
 from flask_cors import CORS
-from config import logs
+from config import logs,all_data
 from flask_talisman import Talisman
 
 app = Flask(__name__)
@@ -13,17 +13,46 @@ csp = {
     'default-src': '\'self\'',
     'style-src': [
         '\'self\'',
-        'https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css',  # Tailwind CSS CDN
+        'https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css',
     ],
-    # Add more directives as needed
 }
 
 talisman = Talisman(
     app,
     content_security_policy=csp,
-    force_https=False,  # Set to True if you want to force HTTPS
+    force_https=False, 
 )
 
+@app.route('/register',methods = ['GET'])
+def login():
+    return render_template('login.html'),200
+
+@app.route('/register', methods=['POST'])
+def registerPost():
+    bot_id = request.form.get('bot_id')
+    client_id = request.form.get('client_id')
+    client_secret = request.form.get('client_secret')
+    eventname = request.form.get('eventname')
+
+    if client_id:
+        auth_url = "https://api.particle.io/oauth/token"
+        payload = f'grant_type=client_credentials&client_id={client_id}&client_secret={client_secret}'
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        response = requests.request("POST", auth_url, headers=headers, data=payload)
+        response_data = response.json()
+        if response.status_code == 200:
+           webhook_url = f"https://api.telegram.org/bot{bot_id}/setWebhook?url=https://particle-api-d8i53.ondigitalocean.app/{bot_id}/{response_data.get('access_token')}/{eventname}"
+           payload = {}
+           headers = {}
+           response = requests.request("POST", webhook_url, headers=headers, data=payload)
+           if response.status_code == 200:
+               all_data[bot_id] = {'bot_id': bot_id ,'access_token':response_data.get('access_token'),'event_name':eventname}
+               return render_template('successful.html', data=all_data[bot_id])
+           return render_template('error.html',error = "Telegram Bot setWebhook API Error!" ,sub = f"Please verify Telegram Bot ID : {bot_id}")
+        return render_template('error.html',error = "Particle AuthToken Generation error!" , sub = f"Please verify the following :<br>Client ID : {client_id}<br>Client Secret : {client_secret}")       
+    return render_template('error.html',error = "Invalid Entries! Please try again" , sub = "")     
+
+    
 @app.route('/<botid>/<accesstoken>/<eventname>',methods=['POST'])
 def update(botid,accesstoken,eventname):
     data = request.get_json()
@@ -53,12 +82,18 @@ def update(botid,accesstoken,eventname):
     return 'Data successfully sent to particle device',200
 
 
-@app.route('/debug')
+@app.route('/debug/logs')
 def debug():
     if logs:
         return render_template('debug.html', logs = logs),200
     return ("Oops ! No logs to display"), 200
-  
+
+@app.route('/debug/admin')
+def admin():
+    if all_data:
+        return render_template('admin.html', data = all_data),200
+    return ("Oops ! No registrations yet "), 200
+
 if __name__ == '__main__':
     app.run()
 
